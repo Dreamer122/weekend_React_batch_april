185cf468198455e0990117843fa74ab21102f9f2
dreamer infotech <infotechdreamer@gmail.com>
2025-05-03 12:26:33 +0530

    home added

---
+++ Second_project/src/Components/Card.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export const Card = () => {
+  return (
+    <>
+    
+    <div className='card'>
+        <img src="" alt="" />
+        <p>title</p>
+        <p>price</p>
+
+        </div>
+        </>
+  )
+}
--- Second_project/src/pages/Home.jsx
+++ Second_project/src/pages/Home.jsx
@@ -1,23 +1,33 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router'
 
 export const Home = () => {
+	const url = 'https://youtube-data8.p.rapidapi.com/auto-complete/?q=cartoon&hl=en&gl=US';
+const options = {
+	method: 'GET',
+	headers: {
+		'x-rapidapi-key': '221a169e29msh3263d9eb3ab50e5p137ef9jsna217e6a58a33',
+		'x-rapidapi-host': 'youtube-data8.p.rapidapi.com'
+	}
+};
+	
+	const callApi= async()=>{
+
+	try {
+		const response = await fetch(url, options);
+		const result = await response.json();
+		console.log(result);
+	} catch (error) {
+		console.error(error);
+	}
+}
+
+	useEffect(()=>{
+		callApi()
+	},[])
   return (
     <div>
-        <main className="center-content">
-	<h1>
-		Doel Javid is:
-		<div className="h1-scroll-div">
-			<div className="h1-scroll-content">
-				<div aria-label="a programmer,">a Programmer</div>
-				<div aria-label="an artist,">an Artist</div>
-				<div aria-label="a designer.">a Designer</div>
-				<div aria-hidden="true">a Baker</div>
-				<div aria-hidden="true">a Programmer</div>
-			</div>
-		</div>
-	</h1>
-</main>
+      <h1>Home page</h1>
   <div style={{textAlign:"center"}}>
  <Link to={"/login"}> <button style={{height:"40px",width:"150px",}}>login</button></Link>
   </div>
